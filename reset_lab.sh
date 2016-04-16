540bda10d9c6bdc0970164d7f0c31143dbf3ea2a
David Murphy <david.b.murphy.tx@gmail.com>
2016-04-16 21:28:35 +0100

    Change mongo_dbpath_root to be  ./labs/ not /labs

--- reset_lab.sh
+++ reset_lab.sh
@@ -8,4 +8,4 @@ fi
 rm -rf ${mongo_dbpath_root}/* ${start_dir}/config_dump
 killall mongos mongod
 rm -rf ${start_dir}/3.0 ${start_dir}/3.2
-${start_dir}/set_bin_dir.sh ./3.0/bin /labs
+${start_dir}/set_bin_dir.sh ./3.0/bin labs
