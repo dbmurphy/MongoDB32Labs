0e6048a55454fe70a624922a73c328eb69a76263
David Murphy <dbmurphy@users.noreply.github.com>
2016-04-16 21:53:06 +0100

    Update reset_lab.sh
    Adding clean up of download files if  the wget script fails.

--- reset_lab.sh
+++ reset_lab.sh
@@ -5,7 +5,7 @@ then
 	exit 255
 fi
 
-rm -rf ${mongo_dbpath_root}/* ${start_dir}/config_dump
+rm -rf ${mongo_dbpath_root}/* ${start_dir}/config_dump ${start_dir}/mongo*.tar ${start_dir}/mongo*.tgz
 killall mongos mongod
 rm -rf ${start_dir}/3.0 ${start_dir}/3.2
 ${start_dir}/set_bin_dir.sh ./3.0/bin labs
