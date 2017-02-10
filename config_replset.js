7a6ec96dbef77f1cd58cb18a89ee5e30c7ced5c1
David Murphy <dbmurphy@users.noreply.github.com>
2017-02-10 12:49:00 +0000

    add arbiter to replset

--- config_replset.js
+++ config_replset.js
@@ -2,6 +2,7 @@
 	port=parseInt(db.adminCommand("getCmdLineOpts").parsed.net.port)
 	port2=port+1;
 	port3=port+2;
+	port4=port+3
 
 	conf = {
 		_id : replSet,
@@ -9,6 +10,7 @@
 			{ _id:0 , host:"localhost:"+port,priority:10},
 			{ _id:1 , host:"localhost:"+port2},
 			{ _id:2 , host:"localhost:"+port3},
+			{ _id:3 , host:"localhost:"+port4, arbiterOnly:true},
 		]
 	};	
 		
